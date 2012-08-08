67be399d8f8b2146a9955e8f0050a9b29bf566c8
shukai888 <shukai888@gmail.com>
2012-08-08 10:29:30 +0800

    start hour recycle

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   
-  http_basic_authenticate_with :name => "dhh", :password => "secret", :except => [:index, :show]
+  http_basic_authenticate_with :name => "dhh", :password => "dhhh", :except => [:index, :show]
+#  http_basic_authenticate_with :name => "frnk", :password => "dfff", :except => [:index, :show]
+
   
   # GET /posts
   # GET /posts.json
